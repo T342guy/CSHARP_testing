b15e16d7ef54b984e91414ec02cf2337e737183e
T342 <chaircomputeroverdrive@gmail.com>
2025-07-25 16:51:03 -0700

    moar playing around

--- program.cs
+++ program.cs
@@ -1,1 +1,13 @@
-﻿console.writeline("hello");
+﻿using Console = Log73.Console;
+using Log73;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Options.LogLevel = LogLevel.Debug;
+Console.Log("You can");
+MessageTypes.Error.Style.Invert = true;
+Console.Error("log customized messages");
+Console.Warn("with Log73!");
+
+Console.Log("Log73 is a simple logging library for .NET applications.");
